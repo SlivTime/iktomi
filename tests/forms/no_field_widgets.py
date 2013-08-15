958bc4e0655d87d3f7300fd19c3a9a43b741b4a4
Harut <yes@harutune.name>
2013-08-15 18:07:30 +0400

    simple tests for no_field_widgets

---
+++ tests/forms/no_field_widgets.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+import unittest
+from os import path
+from html5lib import HTMLParser, treebuilders
+from webob.multidict import MultiDict
+from iktomi.utils.storage import VersionedStorage
+from iktomi.templates import Template, BoundTemplate
+from iktomi.templates import jinja2 as jnj
+from iktomi.templates.jinja2 import TemplateEngine
+import jinja2
+import xpath
+
+from iktomi.forms import fields, convs, widgets, media, perms, \
+                         Form, Field, FieldList, FieldSet
+
+
+class TestFormClass(unittest.TestCase):
+    def setUp(self):
+        pass
+    
+    @property
+    def env(self):
+        DIR = jnj.__file__
+        DIR = path.dirname(path.abspath(DIR))
+        TEMPLATES = [path.join(DIR, 'templates')]
+
+        jinja_loader = TemplateEngine(TEMPLATES)
+        template_loader = Template(engines={'html': jinja_loader},
+                                            *TEMPLATES)
+        env = VersionedStorage()
+        env.template = BoundTemplate(env, template_loader)
+        return env
+
+    def parse(self, value):
+        p = HTMLParser(tree=treebuilders.getTreeBuilder("dom"))
+        return p.parseFragment(value)
+
+
+class TestNoFieldWidget(TestFormClass):
+
+    def test_init_form(self):
+        class TestForm(Form):
+            fields = [
+                widgets.FieldBlock('', fields=[
+                    widgets.NoFieldWidget(),
+                    Field('f1'),
+                    FieldSet('f2', fields=[]),
+                    FieldList('f3', field=Field(''))
+                ]),
+                Field('f4'),
+                FieldSet('f5', fields=[]),
+                FieldList('f6', field=Field('')),
+                widgets.NoFieldWidget(),
+            ]
+
+        form = TestForm()
+        self.assertEqual([x.name for x in form.fields],
+                         ['f1', 'f2', 'f3', 'f4', 'f5', 'f6'])
+        self.assertEqual([type(x) for x in form.widgets],
+                         [widgets.FieldBlock, Field.widget, FieldSet.widget, 
+                          FieldList.widget, widgets.NoFieldWidget])
+
+    def test_init_fieldset(self):
+        class TestForm(Form):
+            fields = [
+                FieldSet('name', fields=[
+                    widgets.FieldBlock('', fields=[
+                        widgets.NoFieldWidget(),
+                        Field('f1'),
+                        FieldSet('f2', fields=[]),
+                        FieldList('f3', field=Field(''))
+                    ]),
+                    Field('f4'),
+                    FieldSet('f5', fields=[]),
+                    FieldList('f6', field=Field('')),
+                    widgets.NoFieldWidget(),
+                ])
+            ]
+
+        form = TestForm()
+        self.assertEqual([x.name for x in form.get_field('name').fields],
+                         ['f1', 'f2', 'f3', 'f4', 'f5', 'f6'])
+        self.assertEqual([type(x) for x in form.get_field('name').widgets],
+                         [widgets.FieldBlock, Field.widget, FieldSet.widget, 
+                          FieldList.widget, widgets.NoFieldWidget])
+
+
+
