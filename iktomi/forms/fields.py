7612a35cbfcc96bdcf060aff04e909ad1f3cb2cc
Harut <yes@harutune.name>
2013-08-16 14:02:10 +0400

    Field(render_type=...) is obsolete

--- iktomi/forms/fields.py
+++ iktomi/forms/fields.py
@@ -23,7 +23,8 @@ class BaseField(object):
     '''
 
     # obsolete parameters from previous versions
-    _obsolete = frozenset(['default', 'get_default', 'template', 'media'])
+    _obsolete = frozenset(['default', 'get_default', 'template', 'media',
+                           'render_type'])
 
     #: :class:`FieldPerm` instance determining field's access permissions.
     #: Can be set by field inheritance or throught constructor.
